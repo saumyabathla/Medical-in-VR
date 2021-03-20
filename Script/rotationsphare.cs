82e1bf4a990022762ccc9f47a2ac92a4b11931d8
Saumya Bathla <51361448+saumyabathla@users.noreply.github.com>
2021-03-21 03:14:41 +0530

    Add files via upload

---
+++ Script/rotationsphare.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotationsphare : MonoBehaviour
+{
+    Vector3 movement;
+    // Start is called before the first frame update
+    void Start()
+    {
+        movement = new Vector3(0, 100, 0);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(movement*Time.deltaTime);   
+    }
+}
---
+++ Script/scenechanger.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class scenechanger : MonoBehaviour
+{
+   public void scene1()
+    {
+        SceneManager.LoadScene("Assistant");
+    }
+    public void scene2()
+    {
+        SceneManager.LoadScene("Main_Scenes");
+    }
+    
+}
